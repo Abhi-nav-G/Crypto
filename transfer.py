8b9145fbd7f1a13935688360e2702e9f1b34ed4a
Abhi-nav-G <g.abhinav1205@gmail.com>
2021-12-28 18:33:54 +0530

    Add user interface for sending coin

--- transfer.py
+++ transfer.py
@@ -6,11 +6,18 @@ class Transfer:
         amount = int(input("Enter the amount of coin you have to send: "))
         p2.coin += amount
         self.coin -= amount
-
-achu = Transfer('achu')
 ashwin = Transfer('ashwin')
+achu = Transfer('achu') 
+quest = input("whom do you want to send money: ")
+if quest == "achu":
+   ashwin.send(achu)
+elif quest == "ashwin":
+    achu.send(ashwin)
+else:
+    print("no other user created")
 
-achu.send(ashwin)
+
+
 print("Balance of achu: ",achu.coin)
 print("Balance of ashwin: ", ashwin.coin)
 
