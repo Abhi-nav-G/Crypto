507eef62fe13af2fc89b8f0c0c633e472dd3aeab
Abhi-nav-G <g.abhinav1205@gmail.com>
2021-12-29 10:09:34 +0530

    Now able to buy coins from main server

--- transfer.py
+++ transfer.py
@@ -1,23 +1,48 @@
 class Transfer:
     def __init__(self,name):
         self.name = name
-        self.coin = 10
+        if self.name != "main_db":
+            self.coin = 10
+        else:
+            self.coin = 100
     def send(self, p2):
         amount = int(input("Enter the amount of coin you have to send: "))
         p2.coin += amount
         self.coin -= amount
+    def buy(self, p2):
+        cbuy = int(input("Enter how many coins you have to buy: "))
+        p2.coin+= cbuy
+        self.coin -= cbuy
+    
+main_db = Transfer("main_db")
 ashwin = Transfer('ashwin')
 achu = Transfer('achu') 
-quest = input("whom do you want to send money: ")
-if quest == "achu":
-   ashwin.send(achu)
-elif quest == "ashwin":
-    achu.send(ashwin)
-else:
-    print("no other user created")
+while True:
+    question = input("Do you want to buy coin(y/n): ")
+    if question == "y":
+        user = eval(input("Enter username: "))
+        main_db.buy(user)
 
 
 
-print("Balance of achu: ",achu.coin)
-print("Balance of ashwin: ", ashwin.coin)
+    if question == "n":
+        quest = input("do you want to send money(y/n): ")
+        if quest == "y":
+            quest = input("whom do you want to send money: ")
+            if quest == "achu":
+                ashwin.send(achu)
+            elif quest == "ashwin":
+                achu.send(ashwin)
+            else:
+                print("no other user created")
+        elif quest == "n":
+            print('bye-bye')
 
+
+
+    print("Balance of achu: ",achu.coin)
+    print("Balance of ashwin: ", ashwin.coin)
+
+    yn = input("Do you want to send again")
+    if yn == "n":
+        break
